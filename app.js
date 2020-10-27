85f72d1cbcf52d256269979ced0a176b0c9a45b2
Sam Cutler <sam.cutler@gmail.com>
2020-10-26 18:54:18 -0700

    a lot of progress, about to attempt medium-sized change to how to render the page

--- app.js
+++ app.js
@@ -7,23 +7,37 @@ const store = {
   questions: [
     {
       question: 'Evolution is defined by its action on which level of organization?',
+      questionShorthand: 'evolution',
       answers: [
         'molecules and genes',
         'communities',
         'population',
         'individual organisms'
       ],
+      answerVals: [
+        'mols-genes',
+        'communities',
+        'population',
+        'individual-org'
+      ],
       correctAnswer: 'population'
     },
     {
       question: 'Which of the following options describes the chemical reaction by which a monomer is attached to a polymer?',
+      questionShorthand: 'monomer-polymer',
       answers: [
         'Hydrolysis',
         'Fermentation',
         'Oxidation/reduction',
         'Dehydration synthesis'
       ],
-      correctAnswer: 'Dehydration synthesis'
+      answerVals: [
+        'hydrolysis',
+        'fermentation',
+        'redox',
+        'dehydration'
+      ],
+      correctAnswer: 'dehydration'
     }
   ],
   quizStarted: false,
@@ -64,11 +78,35 @@ function quizStartHTML() {
 
 function quizQuestionHTML() {
   console.log('The quizQuestionHTML function ran.');
+
+  let currentQuestion = store.questions[(store.questionNumber-1)]
+  
+  
+  return `
+    <form action="#">
+      <h3 class="js-test-click">${currentQuestion.question}</h3>
+      
+      <div class="questions-container">
+        <!-- I should make a function to create this repetition... -->
+        <input type="radio" name='${currentQuestion.questionShorthand}' value='${currentQuestion.answerVals[0]}'>
+         <label for="${currentQuestion.answerVals[0]}">${currentQuestion.answers[0]}</label><br>
+        <input type="radio" name='${currentQuestion.questionShorthand}' value='${currentQuestion.answerVals[1]}'>
+         <label for="${currentQuestion.answerVals[1]}">${currentQuestion.answers[1]}</label><br>
+        <input type="radio" name='${currentQuestion.questionShorthand}' value='${currentQuestion.answerVals[2]}'>
+         <label for="${currentQuestion.answerVals[2]}">${currentQuestion.answers[2]}</label><br>
+        <input type="radio" name='${currentQuestion.questionShorthand}' value='${currentQuestion.answerVals[3]}'>
+         <label for="${currentQuestion.answerVals[3]}">${currentQuestion.answers[3]}</label><br>
+      </div>
+
+      <button class='js-submit-answer-button'>Submit answer.</button>
+    </form>  
+  `
+
+  /*
   return `
     <form action="#">
       <h3 class="js-test-click">Evolution is defined by its action on which level of organization?</h3>
       
-      <!--
       <div class="questions-container">
         <input type="radio" name='evolution' value='mols-genes'>
          <label for="mols-genes">molecules and genes</label><br>
@@ -79,11 +117,11 @@ function quizQuestionHTML() {
         <input type="radio" name='evolution' value='orgs'>
          <label for="orgs">individual organisms</label><br>
       </div>
-      -->
 
       <button class='js-submit-answer-button'>Submit answer.</button>
     </form>  
   `
+  */
 }
 
 function quizCompleteHTML() {
@@ -159,17 +197,50 @@ function handleStartQuiz() {
   });
 }
 
+// ...a correct answer being submitted
+function handleCorrectAnswer() {
+  console.log('Ran handleCorrectAnswer function.');
+
+  // Need to...
+
+  // ...tick up score
+  // ...generate a message (and HTML component?) to declare "you answered correctly"
+  // ...change button to a "next question" button
+
+}
+
 // ...clicks of the "Submit Answer" button
 function handleAnswerSubmission() {
   console.log('Ran handleAnswerSubmission function.')
+  
   $('.js-quiz-page').on('click', '.js-submit-answer-button', function(event) {
-    //event.preventDefault();
+    event.preventDefault();
     console.log('You submitted an answer.');
+
+    // identify the selected answer choice    
+    const answerChoice = $("input[type='radio'][name='evolution']:checked").val();
+    console.log(`You selected "${answerChoice}".`);
+    
+    const currentQuestion = store.questions[(store.questionNumber-1)];
+    const correctAnswer = currentQuestion.correctAnswer;
+
+    // Determine if the answer was correct or not:
+    if (answerChoice === correctAnswer) {
+      console.log('You chose the correct answer!');
+      // Run the to-be-written "correct answer" function.
+      handleCorrectAnswer();
+
+    } else {
+      console.log('You chose the incorrect answer.');
+      // Run the to-be-written "incorrect answer" function.
+    };
+    
     //renderQuizPage();
+
   });
 }
 
-// ...a correct answer being submitted
+
 
 // ...an incorrect answer being submitted
 
